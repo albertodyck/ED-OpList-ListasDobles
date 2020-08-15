80927be13f1ee1293ada9589ca87480ca27559aa
Jose Alberto Dyck Rincon <jose_dyck@unikino.edu.mx>
2020-08-14 21:36:59 -0700

    Agregar archivos de proyecto.

---
+++ ED-OpList-ListasDobles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ED_OpList_ListasDobles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
